662d4f4eec2623e2d3101ed210e63a45fc045734
Daniel Sidorowicz <danielsidorowicz@Mac-mini-24.local>
2024-03-15 10:50:40 +0100

    pensive

---
+++ mapMaker/src/modules/main.ts
@@ -0,0 +1,7 @@
+import '../style/style.css'
+import './mouseSelect.ts'
+
+// (document.querySelector('.app') as HTMLElement).addEventListener("mousedown", function () {
+//     console.log("click");
+
+// })
---
+++ mapMaker/src/modules/options.ts
@@ -0,0 +1,7 @@
+export default class Options {
+    constructor() {
+        const mapOptions = <HTMLElement>document.querySelector("#mapOptions")
+        console.log(mapOptions);
+
+    }
+}
